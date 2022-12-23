050644c836ff8e162150e75b5f22ed3f68ffd2e0
hy3na <hyena459@gmail.com>
2022-12-23 16:37:05 +0900

    GAまわりの試行錯誤

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,11 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://example.com',
   integrations: [mdx(), sitemap(), image(), tailwind(), partytown({
     // Adds dataLayer.push as a forwarding-event.
     config: {
       forward: ["dataLayer.push"],
     },
-  })
-]
+  }),
+],
 });
