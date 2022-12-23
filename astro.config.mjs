b492534ca18c8e7c7cdcc11cbf26570330986fa4
hy3na <hyena459@gmail.com>
2022-12-23 13:37:47 +0900

    GAタグ導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import partytown from '@astrojs/partytown';
 // https://astro.build/config
 import image from "@astrojs/image";
 
@@ -11,5 +11,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), image(), tailwind()]
+  integrations: [mdx(), sitemap(), image(), tailwind(), partytown()]
 });
