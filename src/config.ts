ed248bfc36785382cabbccdf9f6e157110c34cdf
hy3na <hyena459@gmail.com>
2023-01-14 23:27:33 +0900

    og画像動的作成

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'ブログが続かない';
 export const SITE_DESCRIPTION = '愚行録';
+export const SITE_EYE_CATCH_BACKGROUND= 'https://images.microcms-assets.io/assets/e019a2f77056413a90c121880627a1d8/b8d2314077984d0cb879a37a82ed9d54/ogbackground.webp';
