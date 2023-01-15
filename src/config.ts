40e1e130659ce186a2ef67d2ccc4c6e1a328edc5
hy3na <hyena459@gmail.com>
2023-01-15 15:53:58 +0900

    OGP画像動的作成リファクタリング

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,6 @@
 
 export const SITE_TITLE = 'ブログが続かない';
 export const SITE_DESCRIPTION = '愚行録';
-export const SITE_EYE_CATCH_BACKGROUND= 'https://images.microcms-assets.io/assets/e019a2f77056413a90c121880627a1d8/b8d2314077984d0cb879a37a82ed9d54/ogbackground.webp';
+export const SITE_OG_BACKGROUND = 'https://images.microcms-assets.io/assets/e019a2f77056413a90c121880627a1d8/b8d2314077984d0cb879a37a82ed9d54/ogbackground.webp';
+export const SITE_OG_TITLE_IMAGE = "https://assets.imgix.net/~text?txtsize=48&w=1000&h=400&txt-align=center%2Cmiddle&txtfont=Hiragino+Sans+W6&txt=";
+export const SITE_OG_MARK_PARAMETER = "?w=1200&mark-align=center%2Cmiddle&mark64=";
